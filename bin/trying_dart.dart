10c07cfd8b17cdbdb6af4bfd0b4ad159fc5e7c59
Luke Sullivan <luke@sukelully.dev>
2025-11-29 20:05:24 +0000

    finish 9 dart

--- bin/trying_dart.dart
+++ bin/trying_dart.dart
@@ -1,7 +1,5 @@
-import 'package:trying_dart/_07.dart' as test;
+import 'package:trying_dart/_09.dart' as test;
 
 void main(List<String> arguments) {
-  final a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
-  final result = test.returnEvenNumbers(a);
-  print(result);
+  test.guessMyNumber();
 }
---
+++ lib/_09.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+import 'dart:io';
+
+final _random = Random();
+
+void guessMyNumber() {
+  int count = 1;
+  int randNum = _random.nextInt(99) + 1;
+  print("I'm thinking of a number between 1 and 100. Take a guess!");
+  String input = stdin.readLineSync() ?? "";
+  int? guess = int.tryParse(input);
+
+  while (guess != randNum) {
+    if (guess != null && guess < randNum) {
+      print("Higher. Try again.");
+      count++;
+    } else if (guess != null) {
+      print("Lower. Try again!");
+      count++;
+    }
+    input = stdin.readLineSync() ?? "";
+    guess = int.tryParse(input);
+  }
+  print("Correct! It took you $count guesses.");
+}
