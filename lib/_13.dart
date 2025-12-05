38e7056b53d5ae1b95ef8ad27ad952934914beef
Luke Sullivan <luke@sukelully.dev>
2025-12-05 18:12:47 +0000

    use set for 13

--- lib/_13.dart
+++ lib/_13.dart
@@ -1,7 +1,3 @@
 List<int> removeDuplicates(List<int> input) {
-  List<int> noDupes = [];
-  for (var i = 0; i < input.length; i++) {
-    if (!noDupes.contains(input[i])) noDupes.add(input[i]);
-  }
-  return noDupes;
+  return input.toSet().toList();
 }
