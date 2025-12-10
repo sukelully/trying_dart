d0788c95c34d621dd540fdb24f76ef3b6634cada
Luke Sullivan <luke@sukelully.dev>
2025-12-10 07:12:40 +0000

    progress

--- lib/_15.dart
+++ lib/_15.dart
@@ -30,6 +30,11 @@ final chars = List<String>.generate(52, (index) {
   }
 });
 
+final numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+final charPool = [symbols, chars, numbers];
+
 String generatePassword(int length) {
+  String password = '';
+  for (final arr in charPool) {}
   return 'password';
 }
